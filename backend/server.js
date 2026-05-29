ecd33fee8018bcbbd4da9b00c8dc034d87a2051f
houda-eljirari <eljirarihouda99@gmail.com>
2026-05-29 03:10:22 +0100

    Add GET endpoint for users

--- backend/server.js
+++ backend/server.js
@@ -6,8 +6,35 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+let users = [
+  {
+    id: 1,
+    name: "Houda",
+    email: "houda@gmail.com"
+  }
+];
+
 app.get("/", (req, res) => {
   res.send("Backend is running");
+});
+
+// GET USERS
+app.get("/users", (req, res) => {
+  res.json(users);
+});
+
+// ADD USER
+app.post("/users", (req, res) => {
+
+  const newUser = {
+    id: Date.now(),
+    name: req.body.name,
+    email: req.body.email
+  };
+
+  users.push(newUser);
+
+  res.json(newUser);
 });
 
 const PORT = 5000;
